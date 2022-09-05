05a9c3db8954d1b763e46467f9b99f7e77f9d99c
Brian S <briansukhnandan@gmail.com>
2022-09-05 15:57:13 -0400

    backup/restore functionality complete needs testing

--- halomodmanager.sh
+++ halomodmanager.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Globals
-BACKUP_DIR=~/Documents/MCC\ Backups
+BACKUP_DIR=/home/prophet/Documents/MCC-Backups
 
 #####################
 # Process arguments #
@@ -17,8 +17,7 @@ do
   esac
 done
 
-# Why 343i doesn't use a consistent naming scheme
-# is beyond me.
+# Why 343i doesn't use a consistent naming scheme is beyond me.
 function get_maps_path {
   case $game in
     halo1)
@@ -32,16 +31,51 @@ function get_maps_path {
   esac
 }
 
-# Copies file from associated maps directory to the backup directory.
+# Similarities to the Chinese port of Halo: MCC.
+function toggle_pegasus_mode {
+  echo "Toggling Pegasus mode"
+  if [ -f $path/pegasus ]
+  then
+    rm "$path/pegasus"
+  else
+    touch "$path/pegasus"
+  fi
+}
+
+# Copies all contents from associated maps directory to the backup directory.
 function backup_maps {
   if [ -d "$(get_maps_path)" ]
   then
-    echo "Maps directory found. Creating backup directory in $BACKUP_DIR if it doesn't exist and backing up .map files"
+    echo "Creating backup directory if not exists..."
     mkdir -p $BACKUP_DIR/$game
-    cp -r $(get_maps_path) $BACKUP_DIR/$game
+    printf "Copying files from $(get_maps_path) to $BACKUP_DIR/$game...\n\n"
+    cp $(get_maps_path)* $BACKUP_DIR/$game
+    echo "Backup complete!"
 
   else
     echo "The directory $(get_maps_path) doesn't exist! Please verify your game files through Steam."
+
+  fi
+}
+
+# Should only be called if a backup for the specified game exists.
+function restore_maps_from_backup {
+  if [ -d $BACKUP_DIR/$game ]
+  then
+    # Warn the user about this if they want to take manual intervention.
+    echo "Restoring .map files from $BACKUP_DIR/$game/ to $(get_maps_path)"
+    echo "WARNING: This will nuke the current contents of the $game maps folder and replace it with the backup."
+    read -p "Continue? (y/n)" -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      # Probably want to force overwrite if there exists a file with the same file name already.
+      # The "add" operation will back up all mods each time so no harm in doing this.
+      cp -rf $BACKUP_DIR/$game/* $(get_maps_path)
+      printf "\nRestore complete!\n"
+    fi
+
+  else
+    echo "A backup for $game doesn't exist!"
 
   fi
 }
@@ -59,25 +93,17 @@ then
 
 elif [[ $operation == "backup" ]]
 then
-  # echo "backup $(get_maps_path)"
   backup_maps
 
 elif [[ $operation == "restore" ]]
 then
-  echo "restore"
+  restore_maps_from_backup
 
 elif [[ $operation == "pegasus" ]]
 then
-  echo "Toggling Pegasus mode"
-  if [ -f $path/pegasus ]
-  then
-    rm "$path/pegasus"
-  else
-    touch "$path/pegasus"
-  fi
+  toggle_pegasus_mode
 
 else
   echo "Operation not valid!"
-  exit
 
 fi
